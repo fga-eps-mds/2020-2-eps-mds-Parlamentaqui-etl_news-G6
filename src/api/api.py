fba5179a264510455723d6846441a2c8d2ee0e85
jplyra <jplyra97@gmail.com>
2021-03-17 14:12:08 -0300

    Listas de noticias ordenadas por data. Signed-off-by: jplyra <jplyra97@gmail.com> Signed-off-by: csafurtado <cristiansafurtado@gmail.com> Co-authored-by: jplyra <jplyra97@gmail.com> Co-authored-by: csafurtado <cristiansafurtado@gmail.com>
    Signed-off-by: jplyra <jplyra97@gmail.com>

--- src/api/api.py
+++ src/api/api.py
@@ -1,5 +1,7 @@
+import json
 from flask import Blueprint
 from models import *
+from operator import attrgetter
 
 api = Blueprint('api', __name__, url_prefix='/api')
 
@@ -10,3 +12,17 @@ def index():
         deputies = deputies + ' ' + deputy.name
 
     return deputies
+
+@api.route('/news')
+def news():
+    #Pegar as duas noticias mais recentes do nosso banco de dados
+    all_news = News.objects
+    
+    #Ordenar a lista de acordo com a data.
+    sorted_list = sorted(all_news, key=attrgetter('update_date'))
+
+    #criar o json dessas noticias
+    # news_1_json = magic(sorted_list[0])
+    # news_2_json = magic(sorted_list[1])
+    
+    return sorted_list[0].abstract
