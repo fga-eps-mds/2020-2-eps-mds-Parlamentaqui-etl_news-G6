038dcd997889c01fc1f6b87c24ac1bc80b87b31e
jplyra <jplyra97@gmail.com>
2021-03-18 13:05:49 -0300

    Adicionado metodos para transformação em json nas classes de models.py e alteração da lógica do api.py. Agora, os métodos retornam corretamente o json. Co-authored-by: jplyra <jplyra97@gmail.com> Co-authored-by: csafurtado <cristiansafurtado@gmail.com>
    Signed-off-by: jplyra <jplyra97@gmail.com>

--- src/api/api.py
+++ src/api/api.py
@@ -7,11 +7,14 @@ api = Blueprint('api', __name__, url_prefix='/api')
 
 @api.route('/deputies')
 def index():
-    deputies = ''
+    full_json = {}
+    cont = 0
     for deputy in Deputy.objects:
-        deputies = deputies + ' ' + deputy.name
+        temp_json = deputy.to_json(deputy)
+        full_json[cont] = temp_json
+        cont += 1
 
-    return deputies
+    return full_json
 
 @api.route('/news')
 def news():
@@ -20,37 +23,11 @@ def news():
     
     #Ordenar a lista de acordo com a data.
     sorted_list = sorted(all_news, key=attrgetter('update_date'))
+    news_1 = sorted_list[0].to_json(sorted_list[0])
+    news_2 = sorted_list[1].to_json(sorted_list[1])
+    json_full = {
+        'news_01':news_1,
+        'news_02':news_2
+    }
 
-    #criar o json dessas noticias
-    news_1_json = get_json_by_news_class(sorted_list[0])
-    news_2_json = get_json_by_news_class(sorted_list[1])
-    
-    #lista com os dois json
-    full_json = [news_1_json, news_2_json]
-    #cria um json com a lista de jsons
-    return json.dumps(full_json)
-
-#Classe necessária pra transformar em json
-#Link do tutorial: https://stackoverflow.com/questions/3768895/how-to-make-a-class-json-serializable
-class JsonClass():
-    def toJSON(self):
-        return json.dumps(self, default=lambda o: o.__dict__, sort_keys=True, indent=4)
-
-#Retorna uma classe de News em um json
-def get_json_by_news_class(news_class):
-    #cria uma classe temporária dessa nova classe
-    temp_class = JsonClass()
-
-    #adiciona os atributos a serem serializados com o mesmo nome dos atributos da classe passada
-    temp_class.id = news_class.id
-    temp_class.deputy_id = news_class.deputy_id
-    temp_class.link = news_class.link
-    temp_class.photo = news_class.photo
-    temp_class.title = news_class.title
-    temp_class.abstract = news_class.abstract
-    temp_class.deputy_name = news_class.deputy_name
-    temp_class.update_date = f"{news_class.update_date}"
-    temp_class.source = news_class.source
-
-    #transforma essa temp_class para um json.
-    return temp_class.toJSON()
+    return json_full
--- src/models.py
+++ src/models.py
@@ -20,6 +20,28 @@ class Deputy(Document):
     twitter_username = StringField()
     facebook_username = StringField()
 
+    def to_json(self, context):
+        return{
+            'id':context.id,
+            'name':context.name,
+            'photo_url':context.photo_url,
+            'initial_legislature_id':context.initial_legislature_id,
+            'final_legislature_id':context.final_legislature_id,
+            'initial_legislature_year':context.initial_legislature_year,
+            'final_legislature_year':context.final_legislature_year,
+            'last_activity_date':context.last_activity_date,
+            'full_name':context.full_name,
+            'sex':context.sex,
+            'email':context.email,
+            'birth_date':context.birth_date,
+            'death_date':context.death_date,
+            'federative_unity':context.federative_unity,
+            'party':context.party,
+            'instagram_username':context.instagram_username,
+            'twitter_username':context.twitter_username,
+            'facebook_username':context.facebook_username
+        }
+
 class News(Document):
     id = IntField(primary_key=True)
     deputy_id = IntField()
@@ -30,6 +52,19 @@ class News(Document):
     deputy_name = StringField()
     update_date = DateTimeField()
     source = StringField()
+
+    def to_json(self, context):
+        return {
+            'id':context.id,
+            'deputy_id':context.deputy_id,
+            'link': context.link,
+            'photo':context.photo,
+            'title':context.title,
+            'abstract':context.abstract,
+            'deputy_name':context.deputy_name,
+            'update_date':context.update_date,
+            'source':context.source
+        }
 
 
 # class Tweet(Document):
